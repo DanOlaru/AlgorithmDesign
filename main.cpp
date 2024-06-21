c8e89ba8cb701101f65ffa73ab0c5a5856cdf125
Dan Olaru <dan.olaru@zuehlke.com>
2024-06-21 23:04:37 +0200

    fix solution

--- main.cpp
+++ main.cpp
@@ -1,39 +1,85 @@
+
 #include <iostream>
 #include <fstream>
-#include "string"
-
 #include "stringDistance.h"
 
 using namespace std;
 
+void print_cost_matrix(char *target, char *search);
+void print_parent_matrix(char *target, char *search);
 
+cell m[MAXLEN + 1][MAXLEN + 1];
 
 int main(int argc, const char *argv[])
 {
+    char *target = " thou shalt not";
+    char *search = " you should not";
 
-    // string line;
-    // fstream inputFile("resources/StringsInput.txt");
-
-    // if (inputFile.is_open()) {
-    //     cout << "Contents " << endl;
-
-    //     while(inputFile.good()) {
-
-    //         cout << line << " ";
-    //         inputFile >> line;
-    //     }
-    //     cout << endl;
-    // } else{
-    //     cout << "Unable to open file " << inputFile.good() << endl;
-    // }
-
-    char *target = "theXrainYoYMP";
-    char *search = "yePrainBPZMP";
-
-    int cost = string_compare_recursive(target, search, strlen(target), strlen(search));
+    // int cost = string_compare_recursive(target, search, strlen(target), strlen(search));
+    int cost = string_compare(target, search, m);
 
     cout << "cost == " << cost << endl;
+    // print_cost_matrix(target, search);
+    print_parent_matrix(target, search);
+
+// printf("len s%d\n", strlen(search));
+// printf("len T%d\n", strlen(target));
+
+    reconstruct_path(target, search, strlen(search)-1, strlen(target)-1, m);
 
     return 0;
 }
 
+void print_cost_matrix(char *target, char *search)
+{
+    printf("   ");
+    for (size_t l = 0; l < strlen(search); l++)
+    {
+        printf(" %c, ", search[l]);
+    }
+    printf("\n");
+    for (size_t i = 0; i < strlen(target); i++)
+    {
+        printf("%c, ", target[i]);
+
+        for (size_t j = 0; j < strlen(search); j++)
+        {
+            // printf("%d-%d, ", m[i][j].cost, m[i][j].parent);
+
+            m[i][j].cost >= 10
+                ? printf("%d, ", m[i][j].cost)
+                : printf(" %d, ", m[i][j].cost);
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+}
+
+void print_parent_matrix(char *target, char *search)
+{
+    printf("    ");
+    for (size_t l = 0; l < strlen(search); l++)
+    {
+        printf("%c, ", search[l]);
+    }
+    printf("\n");
+
+    for (size_t i = 0; i < strlen(target); i++)
+    {
+        printf("%c, ", target[i]);
+
+        for (size_t j = 0; j < strlen(search); j++)
+        {
+            if (m[i][j].parent > 0 && j == 0)
+            {
+                printf(" %d, ", m[i][j].parent);
+            }
+            else
+                printf("%d, ", m[i][j].parent);
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+}
--- stringDistance.h
+++ stringDistance.h
@@ -1,6 +1,18 @@
+#include "string"
+#include <iostream>
+
 #define MATCH 0
 #define INSERT 1
 #define DELETE 2
+#define MAXLEN 100
+
+using namespace std;
+
+typedef struct
+{
+    int cost;
+    int parent;
+} cell;
 
 int indel(char c)
 {
@@ -13,6 +25,124 @@ int match(char c, char d)
         return 0;
     else
         return 1;
+}
+
+void row_init(int i, cell m[MAXLEN + 1][MAXLEN + 1])
+{
+    m[0][i].cost = i;
+
+    if (i > 0)
+    {
+        m[0][i].parent = INSERT;
+    }
+    else
+    {
+        m[0][i].parent = -1;
+    }
+}
+
+void column_init(int i, cell m[MAXLEN + 1][MAXLEN + 1])
+{
+    m[i][0].cost = i;
+
+    if (i > 0)
+    {
+        m[i][0].parent = DELETE;
+    }
+    else
+    {
+        m[i][0].parent = -1;
+    }
+}
+
+void goal_cell(char *s, char *t, int *i, int *j)
+{
+    *i = strlen(s) - 1;
+    *j = strlen(t) - 1;
+}
+
+void insert_out(char *t, int j)
+{
+    printf("I-");
+}
+
+void delete_out(char *t, int j)
+{
+    printf("D-");
+}
+
+void match_out(char *s, char *t, int i, int j)
+{
+    if (s[i] == t[j])
+        printf("M-");
+    else
+        printf("S-");
+}
+
+int string_compare(char *s, char *t, cell m[MAXLEN + 1][MAXLEN + 1])
+{
+    int i, j, k;
+    int opt[3];
+
+    for (i = 0; i < MAXLEN; i++)
+    {
+        row_init(i, m);
+        column_init(i, m);
+    }
+
+    for (i = 1; i < strlen(s); i++)
+    {
+        for (j = 1; j < strlen(t); j++)
+        {
+            opt[MATCH] = m[i - 1][j - 1].cost + match(s[i], t[j]);
+            opt[INSERT] = m[i][j - 1].cost + indel(t[j]);
+            opt[DELETE] = m[i - 1][j].cost + indel(s[i]);
+
+            m[i][j].cost = opt[MATCH];
+            m[i][j].parent = MATCH;
+            for (k = INSERT; k <= DELETE; k++)
+            {
+                if (opt[k] < m[i][j].cost)
+                {
+                    m[i][j].cost = opt[k];
+                    m[i][j].parent = k;
+                }
+            }
+        }
+    }
+
+    goal_cell(s, t, &i, &j);
+    return (m[i][j].cost);
+}
+
+void reconstruct_path(char *s, char *t, int i, int j, cell m[MAXLEN + 1][MAXLEN + 1])
+{
+    if (m[i][j].parent == -1)
+        return;
+
+    // printf("%d %d, \n", i, j);
+    printf("%c %c, \n", s[i], t[j]);
+
+    if (m[i][j].parent == MATCH)
+    {
+        reconstruct_path(s, t, i - 1, j - 1, m);
+        match_out(s, t, i, j);
+        return;
+    }
+
+    if (m[i][j].parent == INSERT)
+    {
+        reconstruct_path(s, t, i, j - 1, m);
+        insert_out(t, j);
+
+        return;
+    }
+    if (m[i][j].parent == DELETE)
+    {
+        reconstruct_path(s, t, i - 1, j, m);
+        delete_out(s, i);
+        return;
+    }
 }
 
 int string_compare_recursive(char *s, char *t, int i, int j)
