158a00cfdc4b98198f78d9c5b22e4ae40a885845
Dan Olaru <danolaru@gmail.com>
2024-06-24 00:10:51 +0200

    style code

--- editDistance/stringDistance.h
+++ editDistance/stringDistance.h
@@ -11,176 +11,175 @@ using namespace std;
 
 typedef struct
 {
-    int cost;
-    int parent;
+  int cost;
+  int parent;
 } cell;
 
-
-char* prepend_space(char *input)
+char *prepend_space(char *input)
 {
-    if (input[0] == ' ')
-    {
-        return input;
-    }
-    else
-    {
-        char *prepended = new char[strlen(input) + 2];
-        strcpy(prepended, " ");
-        strcat(prepended, input);
+  if (input[0] == ' ')
+  {
+    return input;
+  }
+  else
+  {
+    char *prepended = new char[strlen(input) + 2];
+    strcpy(prepended, " ");
+    strcat(prepended, input);
 
-        return prepended;
-    }
+    return prepended;
+  }
 }
 
 int indel(char c)
 {
-    return (1);
+  return (1);
 }
 
 int match(char c, char d)
 {
-    if (c == d)
-        return 0;
-    else
-        return 1;
+  if (c == d)
+    return 0;
+  else
+    return 1;
 }
 
 void row_init(int i, cell m[MAXLEN + 1][MAXLEN + 1])
 {
-    m[0][i].cost = i;
+  m[0][i].cost = i;
 
-    if (i > 0)
-    {
-        m[0][i].parent = INSERT;
-    }
-    else
-    {
-        m[0][i].parent = -1;
-    }
+  if (i > 0)
+  {
+    m[0][i].parent = INSERT;
+  }
+  else
+  {
+    m[0][i].parent = -1;
+  }
 }
 
 void column_init(int i, cell m[MAXLEN + 1][MAXLEN + 1])
 {
-    m[i][0].cost = i;
+  m[i][0].cost = i;
 
-    if (i > 0)
-    {
-        m[i][0].parent = DELETE;
-    }
-    else
-    {
-        m[i][0].parent = -1;
-    }
+  if (i > 0)
+  {
+    m[i][0].parent = DELETE;
+  }
+  else
+  {
+    m[i][0].parent = -1;
+  }
 }
 
 void goal_cell(char *s, char *t, int *i, int *j)
 {
-    *i = strlen(s) - 1;
-    *j = strlen(t) - 1;
+  *i = strlen(s) - 1;
+  *j = strlen(t) - 1;
 }
 
 void insert_out(char *t, int j)
 {
-    printf("I-");
+  printf("I-");
 }
 
 void delete_out(char *t, int j)
 {
-    printf("D-");
+  printf("D-");
 }
 
 void match_out(char *s, char *t, int i, int j)
 {
-    if (s[i] == t[j])
-        printf("M-");
-    else
-        printf("S-");
+  if (s[i] == t[j])
+    printf("M-");
+  else
+    printf("S-");
 }
 
 int string_compare(char *s, char *t, cell m[MAXLEN + 1][MAXLEN + 1])
 {
-    int i, j, k;
-    int opt[3];
+  int i, j, k;
+  int opt[3];
 
-    for (i = 0; i < MAXLEN; i++)
-    {
-        row_init(i, m);
-        column_init(i, m);
-    }
+  for (i = 0; i < MAXLEN; i++)
+  {
+    row_init(i, m);
+    column_init(i, m);
+  }
 
-    for (i = 1; i < strlen(s); i++)
+  for (i = 1; i < strlen(s); i++)
+  {
+    for (j = 1; j < strlen(t); j++)
     {
-        for (j = 1; j < strlen(t); j++)
+      opt[MATCH] = m[i - 1][j - 1].cost + match(s[i], t[j]);
+      opt[INSERT] = m[i][j - 1].cost + indel(t[j]);
+      opt[DELETE] = m[i - 1][j].cost + indel(s[i]);
+
+      m[i][j].cost = opt[MATCH];
+      m[i][j].parent = MATCH;
+      for (k = INSERT; k <= DELETE; k++)
+      {
+        if (opt[k] < m[i][j].cost)
         {
-            opt[MATCH] = m[i - 1][j - 1].cost + match(s[i], t[j]);
-            opt[INSERT] = m[i][j - 1].cost + indel(t[j]);
-            opt[DELETE] = m[i - 1][j].cost + indel(s[i]);
-
-            m[i][j].cost = opt[MATCH];
-            m[i][j].parent = MATCH;
-            for (k = INSERT; k <= DELETE; k++)
-            {
-                if (opt[k] < m[i][j].cost)
-                {
-                    m[i][j].cost = opt[k];
-                    m[i][j].parent = k;
-                }
-            }
+          m[i][j].cost = opt[k];
+          m[i][j].parent = k;
         }
+      }
     }
+  }
 
-    goal_cell(s, t, &i, &j);
-    return (m[i][j].cost);
+  goal_cell(s, t, &i, &j);
+  return (m[i][j].cost);
 }
 
 void reconstruct_path(char *s, char *t, int i, int j, cell m[MAXLEN + 1][MAXLEN + 1])
 {
-    if (m[i][j].parent == -1)
-        return;
+  if (m[i][j].parent == -1)
+    return;
 
-    // printf("%d %d, \n", i, j);
-    printf("%c %c, \n", s[i], t[j]);
+  // printf("%d %d, \n", i, j);
+  printf("%c %c, \n", s[i], t[j]);
 
-    if (m[i][j].parent == MATCH)
-    {
-        reconstruct_path(s, t, i - 1, j - 1, m);
-        match_out(s, t, i, j);
-        return;
-    }
+  if (m[i][j].parent == MATCH)
+  {
+    reconstruct_path(s, t, i - 1, j - 1, m);
+    match_out(s, t, i, j);
+    return;
+  }
 
-    if (m[i][j].parent == INSERT)
-    {
-        reconstruct_path(s, t, i, j - 1, m);
-        insert_out(t, j);
+  if (m[i][j].parent == INSERT)
+  {
+    reconstruct_path(s, t, i, j - 1, m);
+    insert_out(t, j);
 
-        return;
-    }
-    if (m[i][j].parent == DELETE)
-    {
-        reconstruct_path(s, t, i - 1, j, m);
-        delete_out(s, i);
-        return;
-    }
+    return;
+  }
+  if (m[i][j].parent == DELETE)
+  {
+    reconstruct_path(s, t, i - 1, j, m);
+    delete_out(s, i);
+    return;
+  }
 }
 
 int string_compare_recursive(char *s, char *t, int i, int j)
 {
-    int k;      // counter
-    int opt[3]; // cost of the 3 options
-    int lowest_cost;
+  int k;      // counter
+  int opt[3]; // cost of the 3 options
+  int lowest_cost;
 
-    if (i == 0)
-        return (j * indel(' '));
-    if (j == 0)
-        return (i * indel(' '));
+  if (i == 0)
+    return (j * indel(' '));
+  if (j == 0)
+    return (i * indel(' '));
 
-    opt[MATCH] = string_compare_recursive(s, t, i - 1, j - 1) + match(s[i], t[j]);
-    opt[INSERT] = string_compare_recursive(s, t, i, j - 1) + indel(t[j]);
-    opt[DELETE] = string_compare_recursive(s, t, i - 1, j) + indel(s[i]);
+  opt[MATCH] = string_compare_recursive(s, t, i - 1, j - 1) + match(s[i], t[j]);
+  opt[INSERT] = string_compare_recursive(s, t, i, j - 1) + indel(t[j]);
+  opt[DELETE] = string_compare_recursive(s, t, i - 1, j) + indel(s[i]);
 
-    lowest_cost = opt[MATCH];
-    for (k = INSERT; k <= DELETE; k++)
-        if (opt[k] < lowest_cost)
-            lowest_cost = opt[k];
-    return (lowest_cost);
+  lowest_cost = opt[MATCH];
+  for (k = INSERT; k <= DELETE; k++)
+    if (opt[k] < lowest_cost)
+      lowest_cost = opt[k];
+  return (lowest_cost);
 }
